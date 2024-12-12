9cab5a3d23203100767791851720c7fba941c8e7
Alexey Rychagov <rrisinglight@gmail.com>
2024-12-12 06:38:21 +0300

    second task autoformat add commets

--- task2/task2.py
+++ task2/task2.py
@@ -16,10 +16,10 @@ def parse_edges_to_graph(edge_csv: str) -> Dict[int, List[int]]:
 
     return(graph)
 
+
 def count_all_descendants(graph: Dict[int, List[int]], node: int) -> int:
 
     all_descendants = 0
-
     for child in graph[node]:
         all_descendants += 1
         all_descendants += count_all_descendants(graph, child)
@@ -30,11 +30,10 @@ def count_all_descendants(graph: Dict[int, List[int]], node: int) -> int:
 def count_direct_ancestors(graph: Dict[int, List[int]], node: int) -> int:
 
     ancestor_count = 0
-    
     for parent, children in graph.items():
         if node in children:
             ancestor_count += 1
-    
+
     return(ancestor_count)
 
 
@@ -48,25 +47,24 @@ def count_all_ancestors(graph: Dict[int, List[int]], node: int) -> int:
 
     return(all_ancestors)
 
+
 def count_near_nodes(graph: Dict[int, List[int]], node: int) -> int:
-    
+
     near_count = 0
     for parent, children in graph.items():
         if node in children:
             near_count += len(graph[parent]) - 1
-    
+
     return(near_count)
 
 
 def calculate_node_relations(graph: Dict[int, List[int]], node: int) -> List[int]:
 
-    nodes = set(graph.keys()).union(*graph.values())
-
-    row_1 = len(graph[node])
-    row_2 = count_direct_ancestors(graph, node)
-    row_3 = count_all_descendants(graph, node) - row_1
-    row_4 = count_all_ancestors(graph, node) - row_2
-    row_5 = count_near_nodes(graph, node)
+    row_1 = len(graph[node])  # Прямые потомки
+    row_2 = count_direct_ancestors(graph, node)  # Прямые предки
+    row_3 = count_all_descendants(graph, node) - row_1  # Все потомки
+    row_4 = count_all_ancestors(graph, node) - row_2  # Все предки
+    row_5 = count_near_nodes(graph, node)  # Соседи
 
     return([row_1, row_2, row_3, row_4, row_5])
 
@@ -74,7 +72,6 @@ def calculate_node_relations(graph: Dict[int, List[int]], node: int) -> List[int
 def main(edge_csv: str) -> str:
 
     graph = parse_edges_to_graph(edge_csv)
-
     nodes = sorted(set(graph.keys()).union(*graph.values()))
 
     result = []
@@ -82,7 +79,7 @@ def main(edge_csv: str) -> str:
         node_relations = calculate_node_relations(graph, node)
         result.append([node] + node_relations)
 
-    return('\n'.join(' '.join(map(str, row)) for row in result))
+    return('\n'.join(','.join(map(str, row)) for row in result))
 
 
 if __name__ == "__main__":
